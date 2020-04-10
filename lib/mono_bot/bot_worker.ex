2a3c405075070c4944e1b3609ab96e63db7a7f9d
Sierra <iamsi2@protonmail.com>
2020-04-10 16:05:35 +0300

    need to fix the problem with api key remembering

--- lib/mono_bot/bot_worker.ex
+++ lib/mono_bot/bot_worker.ex
@@ -14,23 +14,43 @@ defmodule MonoBot.BotWorker do
   def init(args) do
     Logger.info("Worker inits")
     Process.send_after(self(), :tick, 5000)
-    {:ok, Map.merge(args, %{offset: 0})}
+    {:ok, Map.merge(args, %{offset: 0, api_key: nil})}
   end
 
   def handle_info(:tick, state) do
+    IO.inspect(state.api_key)
     {:ok, updates} = Nadia.get_updates(offset: state.offset)
-    last_update = List.last(updates)
 
+    last_update = List.last(updates)
     get_message(last_update, state)
 
-    offset =
+     offset =
       if last_update do
         last_update.update_id + 1
       else
         state.offset
       end
 
-    new_state = Map.put(state, :offset, offset)
+     api_key =
+       if last_update do
+        message_text =
+          last_update
+        |> Map.get(:message)
+        |> Map.get(:text)
+
+      if String.length(message_text) > 15 do
+        message_text
+      else
+        nil
+      end
+      else
+        state.api_key
+      end
+
+    new_state =
+      state
+      |> Map.put(:offset, offset)
+      |> Map.put(:api_key, api_key)
     {:noreply, new_state}
 
     handle_info(:tick, new_state)
@@ -46,42 +66,31 @@ defmodule MonoBot.BotWorker do
       |> Map.get(:message)
       |> Map.get(:text)
 
+     api_key =
+      if String.length(message_text) > 15 do message_text else nil end
+
     new_state =
       state
       |> Map.put(:message, message_text)
       |> Map.put(:chat_id, @default_chat_id)
       |> Map.put(:username, username)
+      |> Map.put(:api_key, api_key)
 
     handle_cast(:message, new_state)
   end
 
   def handle_cast(:message, %{message: message} = state) do
-    if message === "View Balance" do
-      # {:ok, env} = user(@default_mono_token) |> Tesla.get("https://api.monobank.ua/personal/client-info")
-      # my_info = Tesla.put_header(env, "X-Token", @default_mono_token)
+    IO.inspect(state)
+    cond do
+      message === "View Balance" ->
+        if state[:api_key] !== nil do
+          IO.inspect(state)
+          check_balance(state.api_key, state.chat_id)
+         else
+         Nadia.send_message(state.chat_id, "Send me Mono API key")
+        end
 
-    #  my_info = Tesla.request(client, [{"X-Token", @default_mono_token}])
-
-
-      {:ok, env} =
-      MonoApi.new(@default_mono_token)
-       |>Tesla.get("https://api.monobank.ua/personal/client-info")
-
-      balance_with_cents = env.body
-      |> String.split(",")
-      |> Enum.filter(fn str -> String.contains?(str, "balance") end)
-      |> Enum.map(fn str -> String.split(str, ":") end)
-      |> List.flatten()
-      |> Enum.at(1)
-      |> String.to_integer()
-
-      balance_without_cents = (balance_with_cents - rem(balance_with_cents, 100)) / 100
-
-
-      Nadia.send_message(state.chat_id, "Your current balance is #{balance_without_cents}")
-    else
-
-    Nadia.send_message(
+        true -> Nadia.send_message(
           state.chat_id,
           "Hi, " <>
             state.username <>
@@ -94,9 +103,26 @@ defmodule MonoBot.BotWorker do
             one_time_keyboard: true
           }
         )
+    end
 
-            end
     {:noreply, state}
+  end
+
+  defp check_balance(api_key, chat_id) do
+    {:ok, env} = MonoApi.new(api_key)
+      |>Tesla.get("https://api.monobank.ua/personal/client-info")
+
+    balance_with_cents = env.body
+    |> String.split(",")
+    |> Enum.filter(fn str -> String.contains?(str, "balance") end)
+    |> Enum.map(fn str -> String.split(str, ":") end)
+    |> List.flatten()
+    |> Enum.at(1)
+    |> String.to_integer()
+
+    balance_without_cents = (balance_with_cents - rem(balance_with_cents, 100)) / 100
+
+    Nadia.send_message(chat_id, "Your current balance is #{balance_without_cents}")
   end
 end
 
--- lib/mono_bot/mono_api.ex
+++ lib/mono_bot/mono_api.ex
@@ -1,10 +1,8 @@
 defmodule MonoApi do
   use Tesla
 
-  @default_mono_token "1148214767:AAEsDnbHTRcPuAbrEdWX31GX-5_PZuO_9UU"
-
   plug Tesla.Middleware.BaseUrl, "https://api.monobank.ua/personal/client-info"
-  plug Tesla.Middleware.Headers, [{"X-Token", @default_mono_token}]
+  plug Tesla.Middleware.Headers
   plug Tesla.Middleware.JSON
 
   def new(token) do
