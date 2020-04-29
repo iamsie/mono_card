8b2b1d089b5c8429aded2eb3f34b0731fb9b87c3
Sierra <iamsi2@protonmail.com>
2020-04-29 08:50:21 +0300

    add coonn at the end of create action

--- lib/mono_card_web/controllers/wehook_controller.ex
+++ lib/mono_card_web/controllers/wehook_controller.ex
@@ -8,15 +8,14 @@ defmodule MonoCardWeb.WebhookController do
     conn
   end
 
-  def create(_conn, payload) do
+  def create(conn, payload) do
     Logger.info("Get a payload")
-    IO.inspect(payload)
-    # {:ok, decoded_payload} = Jason.decode(payload)
-    # Logger.info("Payload decoded")
     white_card_id = payload["data"]["account"]
     balance = payload["data"]["statementItem"]["balance"]
 
     if Accounts.exist_by_card_id?(white_card_id) === true && balance < 100_000,
       do: Replier.balance_less_1000(balance, white_card_id)
+
+    conn
   end
 end
