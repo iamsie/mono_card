50e927d66846842423798fe724b3b577975fb6db
Sierra <iamsi2@protonmail.com>
2020-04-29 08:34:03 +0300

    remove the Jason decode from Webhook controller

--- lib/mono_card_web/controllers/wehook_controller.ex
+++ lib/mono_card_web/controllers/wehook_controller.ex
@@ -11,10 +11,10 @@ defmodule MonoCardWeb.WebhookController do
   def create(_conn, payload) do
     Logger.info("Get a payload")
     IO.inspect(payload)
-    {:ok, decoded_payload} = Jason.decode(payload)
-    Logger.info("Payload decoded")
-    white_card_id = decoded_payload["data"]["account"]
-    balance = decoded_payload["data"]["statementItem"]["balance"]
+    # {:ok, decoded_payload} = Jason.decode(payload)
+    # Logger.info("Payload decoded")
+    white_card_id = payload["data"]["account"]
+    balance = payload["data"]["statementItem"]["balance"]
 
     if Accounts.exist_by_card_id?(white_card_id) === true && balance < 100_000,
       do: Replier.balance_less_1000(balance, white_card_id)
